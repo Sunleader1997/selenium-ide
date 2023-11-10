05b2affbfb61977b3a5b69b2b6ba74b88e1d5e7b
Sunleader <sunyaxing_1997@163.com>
2023-11-10 09:56:27 +0800

    ssh 创建完毕

--- packages/selenium-ide/src/browser/windows/MyProject/components/DivShell/ShellEditor.tsx
+++ packages/selenium-ide/src/browser/windows/MyProject/components/DivShell/ShellEditor.tsx
@@ -1,13 +1,9 @@
-import Drawer, {DrawerProps} from "@mui/material/Drawer";
+import Card, {CardProps} from "@mui/material/Card";
 import {styled} from "@mui/material/styles";
 import {Shell} from "@seleniumhq/side-model";
 //import Box from '@mui/material/Box';
 import * as React from "react";
 import {useEffect} from "react";
-import Stack from "@mui/material/Stack";
-import TerminalIcon from "@mui/icons-material/Terminal";
-import Toolbar from "@mui/material/Toolbar";
-import Box from "@mui/material/Box";
 import {Controlled as CodeMirror} from 'react-codemirror2'
 import 'codemirror/lib/codemirror.js';
 import 'codemirror/lib/codemirror.css';
@@ -35,83 +31,70 @@ import 'codemirror/addon/comment/comment.js';
 import 'codemirror/addon/comment/continuecomment.js';
 import './ShellEditorCss.css';
 
-interface ShellEditorProps extends DrawerProps{
+interface ShellEditorProps extends CardProps{
   shell: Shell
+  onContentChanged: (content: string) => void
 }
 
-const ShellEditorCore= styled(Drawer,{})<DrawerProps>(()=>({
+const ShellEditorCore= styled(Card,{})<CardProps>(()=>({
   minWidth: 250
 }))
 
 const ShellEditor:  React.FC<ShellEditorProps> = ({
   shell,
+  onContentChanged,
   ...others
 }) => {
-  const [content, setContent] = React.useState<string>(shell.content)
+  const [currentShell, _setCurrentShell] = React.useState<Shell>(shell)
   useEffect(() => {
     console.log("shell update")
-    setContent(shell.content)
+    _setCurrentShell(shell)
   }, [shell.id]);
+  const setCurrentShellContent = (content: string) => {
+    _setCurrentShell({
+      ...currentShell,
+      content: content
+    })
+  }
   return (
     <ShellEditorCore {...others}>
-      <Toolbar variant="dense" sx={{
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        color: 'white',
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        maxHeight: 22,
-        px: 1
-      }}>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          spacing={0}
-          sx={{height: 22}}
-        >
-          <Stack direction="row" alignItems="center" spacing={0.5} sx={{height: 22, display: 'flex', fontSize: 'larger'}}>
-            <TerminalIcon/> <div>{shell.name}</div>
-          </Stack>
-        </Stack>
-      </Toolbar>
-      <Box component="main" sx={{ p: 0, flex: 1 }}>
-        <CodeMirror
-          className='CodeMirrorFull'
-          value={content}
-          options={{
-            mode: 'text/x-sh', // 代码类型
-            theme: 'material', // 主题
-            indentWithTabs: true, // 在缩进时，是否需要把 n*tab宽度个空格替换成n个tab字符，默认为false 。
-            smartIndent: true, // 自动缩进,回车时跟上一行保持相同缩进
-            lineNumbers: true, // 左侧显示行数
-            matchBrackets: true, // 括号匹配
-            cursorHeight: 1, // 光标高度
-            autoRefresh: true,
-            line: true,
-            placeholder: '请输入脚本',
-            // readOnly: true, // 是否只读
-            hintOptions: {
-              // 自定义提示选项
-              completeSingle: false, // 当匹配只有一项的时候是否自动补全，建议false
-              // hint: handleHintShow,
-              // async: true,
-              // disableKeywords: ['hello'],
-            },
-            extraKeys: {
-              // 触发按键
-              Ctrl: 'autocomplete',
-            }
-          }}// 设置尺寸
-          editorDidMount={(editor) => {
-            editor.setSize('100%', '100%');
-          }}
-          onBeforeChange={(_editor, _data, value) => {
-            setContent(value)
-          }}
-          onChange={(_editor, _data, _value) => {
-            shell.content = _value
-          }}
-        />
-      </Box>
+      <CodeMirror
+        className='CodeMirrorFull'
+        value={currentShell.content}
+        options={{
+          mode: 'text/x-sh', // 代码类型
+          theme: 'material', // 主题
+          indentWithTabs: true, // 在缩进时，是否需要把 n*tab宽度个空格替换成n个tab字符，默认为false 。
+          smartIndent: true, // 自动缩进,回车时跟上一行保持相同缩进
+          lineNumbers: true, // 左侧显示行数
+          matchBrackets: true, // 括号匹配
+          cursorHeight: 1, // 光标高度
+          autoRefresh: true,
+          line: true,
+          placeholder: '请输入脚本',
+          // readOnly: true, // 是否只读
+          hintOptions: {
+            // 自定义提示选项
+            completeSingle: false, // 当匹配只有一项的时候是否自动补全，建议false
+            // hint: handleHintShow,
+            // async: true,
+            // disableKeywords: ['hello'],
+          },
+          extraKeys: {
+            // 触发按键
+            Ctrl: 'autocomplete',
+          }
+        }}// 设置尺寸
+        editorDidMount={(editor) => {
+          editor.setSize('100%', '100%');
+        }}
+        onBeforeChange={(_editor, _data, value) => {
+          setCurrentShellContent(value)
+        }}
+        onChange={(_editor, _data, _value) => {
+          onContentChanged(_value)
+        }}
+      />
     </ShellEditorCore>
   )
 }
--- packages/selenium-ide/src/browser/windows/MyProject/components/DivShell/index.tsx
+++ packages/selenium-ide/src/browser/windows/MyProject/components/DivShell/index.tsx
@@ -1,69 +1,95 @@
 import {CoreSessionData} from "@seleniumhq/side-api";
 import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/Inbox';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Stack from '@mui/material/Stack';
+
+
 import * as React from "react";
-import ShellItem from "browser/windows/MyProject/components/DivShell/ShellItem";
 import ShellEditor from "browser/windows/MyProject/components/DivShell/ShellEditor";
 import {Shell} from "@seleniumhq/side-model";
 import { styled } from '@mui/material/styles';
 
-const drawerWidth = 600;
 interface DivShellProps{
   session: CoreSessionData,
 }
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginRight: -drawerWidth,
-  }),
-  position: 'relative',
-}));
+const Main = styled('main', { })(()=>({
+  minWidth: 250
+}))
 
 const DivShell: React.FC<DivShellProps> = ({
   session,
 })=>{
   const { shells } = session.project;
-  const [activedShell, setActivedShell] = React.useState<Shell>({content: "", id: "", name: ""});
-  const [showFlag, setShowFlag] = React.useState<boolean>(false);
+  const [ activeShell, setActiveShell] = React.useState<Shell>({content: "", id: "", name: ""});
+  const [ openedShells, setOpenedShells] = React.useState<Shell[]>([]);
+  const addOpenedShell = (shell: Shell) => {
+    if(!openedShells.some(obj => obj.id === shell.id)){
+      setOpenedShells([...openedShells, shell])
+    }
+  }
+  const onActiveContentChanged = (content: string)=>{
+    setActiveShell({
+      ...activeShell,
+      content: content
+    })
+  }
+  const handleListItemClick = (_event: any, shell: Shell) => {
+    setActiveShell(shell)
+    addOpenedShell(shell)
+  }
   return (
-    <Box id="divShell" sx={{ display: 'flex' }}>
-      <Main id="shellItemsMain" open={Boolean(activedShell)}>
-        {shells.map(shell => (
-          <ShellItem shell={shell} onClickContent={(event: React.MouseEvent<Shell>)=> {
-            console.log('click',event.currentTarget)
-            setActivedShell(shell);
-            setShowFlag(true)
-          }}/>
-        ))}
+    <Box id="divShell" sx={{ display: 'flex', height: '100%' }}>
+      <Main id="shellItemsMain">
+        <List component="nav" aria-label="main mailbox folders">
+          {shells.map(shell => (
+            <ListItemButton
+              selected={activeShell.id === shell.id}
+              onClick={(event) => handleListItemClick(event, shell)}
+            >
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          ))}
+        </List>
       </Main>
-      <ShellEditor
-        id="ShellEditorPanel"
-        shell={activedShell}
-        anchor="right"
-        sx={{
-         width: drawerWidth,
-         flexShrink: 0,
-         '& .MuiDrawer-paper': {
-           width: drawerWidth,
-         },
-        }}
-        variant="persistent"
-        open={showFlag}
-        onClose={()=>{
-         setShowFlag(false)
-        }}
-      />
+      <Stack spacing={0} sx={{ flex: 1, minWidth: 0, minHeight: 0}}>
+        <Tabs
+          value={shells}
+          onChange={handleListItemClick}
+          variant="scrollable"
+          scrollButtons
+          allowScrollButtonsMobile
+          aria-label="scrollable force tabs example"
+          sx={{ minHeight: '20px', width: '100%' }}
+        >
+          {shells.map(shell => (
+            <Tab
+              value={shell.id}
+              label={shell.name}
+              sx={{
+                background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+                color: 'white',
+                minHeight: '20px'
+              }}
+              wrapped
+            />
+          ))}
+        </Tabs>
+        <ShellEditor
+          id="ShellEditorPanel"
+          shell={activeShell}
+          onContentChanged={onActiveContentChanged}
+          sx={{flex: 1}}
+        />
+      </Stack>
     </Box>
   )
 }
